9fd874aee9840acdf875321ee6fe165b3f557793
Weng-john <59682713+Weng-john@users.noreply.github.com>
2020-01-26 21:33:45 +0800

    first commit

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+var btn = document.getElementById("passBtn");
+btn.onclick = function(){
+    var done = 0;
+    var team = document.login.username.value;
+    var password = document.login.password.value;
+    
+    if (team == "第一小隊" && password == "1"){
+        window.open("index-1.html");
+        done = 1;
+    }
+    if (team == "第二小隊" && password == "2"){
+        window.open("index-2.html");
+        done = 1;
+    }
+    if (team =="第三小隊" && password == "3"){
+        window.open("index-3.html");
+        done = 1;
+    }
+    if (team == "第四小隊" && password == "4"){
+        window.open("index-4.html");
+        done = 1;
+    }
+    if (team == "第五小隊" && password == "5"){
+        window.open("index-5.html");
+        done = 1;
+    }
+    if (done == 0){
+        alert("             輸入錯誤>_<\n                重新找找吧!!!");
+    }
+}
